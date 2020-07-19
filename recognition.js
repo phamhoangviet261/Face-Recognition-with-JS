59bd94e6afa525864033c3c32308b5719ec19a12
Pham Hoang Viet <42289889+ShiroVV-hcmus@users.noreply.github.com>
2020-07-19 16:49:01 +0700

    Ver-2

--- recognition.js
+++ recognition.js
@@ -1,21 +1,24 @@
 //Tạo hàm bất đồng bộ
 const start = async function(){
     //Tải Trained model
-    await faceapi.nets.ssdMobilenetv1.loadFromUri('https://localhost:8080/');
+    await faceapi.nets.ssdMobilenetv1.loadFromUri('http://localost:8080');
 
     //Lấy ảnh
     const inputMLT = document.getElementById('mltImg');
     //Lay kich thuoc anh
-    const displaySizeMLT = {width: inputMLT.width, heigh: 1200};
+    const displaySizeMLT = {width: inputMLT.width, heigh: inputMLT.heigh};
+    displaySizeMLT.heigh = 346;
+    console.log(displaySizeMLT)
     //Lay khung ve
     const mltCanvas = document.getElementById('mltCanvas');
     //Thay kich thuoc khung ve anh phu hop
     faceapi.matchDimensions(mltCanvas, displaySizeMLT);
+    
     mltCanvas.getContext("2d").drawImage(inputMLT, 0, 0);
     //Nhan dang guong mat
     const recognitionFace = await faceapi.detectAllFaces(inputMLT);
     //Ve khung nhan dang guong mat
     faceapi.draw.drawDetections(mltCanvas, recognitionFace);
-};
+}
 
 start();
